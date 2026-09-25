46e1af309dda2f719ac11377b56a13ca9e59c238
NetAcad DEVASC <changeme@example.com>
2026-09-25 04:12:36 +0000

    Primer commit - Examen Final Devnet

---
+++ vlan_check.py
@@ -0,0 +1,11 @@
+print("Nombre Completo: Luis Palacios")
+print("RUT: 17.194.070-2")
+
+vlan = int(input("Ingrese el número de VLAN: "))
+
+if 1 <= vlan <= 1005:
+    print(f"La VLAN {vlan} corresponde a un rango normal.")
+elif 1006 <= vlan <= 4094:
+    print(f"La VLAN {vlan} corresponde a un rango extendido.")
+else:
+    print(f"La VLAN {vlan} no corresponde a una VLAN válida.")
